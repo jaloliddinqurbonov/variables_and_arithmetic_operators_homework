fcc3b11539e5d7d729ef57af955b0a4627fd7a3f
jaloliddinqurbonov <qurbonovjaloliddin2003@gmail.com>
2025-04-21 11:07:30 +0500

    asd

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,8 +1,8 @@
 # Create a variable 'num_one'  and assign it the value of the int 5.
-x=5
-y=8
-z=x+y
-print(z)
+num_one=5
+num_two=8
+variable_total=num_one+num_two
+print(variable_total)
 # Create a variable 'num_two'   and assign it the value of the int 4.
 
 # Create a variable 'variable_total' and assign it the sum of  'num_one' and 'num_two'.
