f8db08236dd4a142a0cf931bcd03567835eb04fc
jaloliddinqurbonov <qurbonovjaloliddin2003@gmail.com>
2025-04-21 11:00:27 +0500

    sdmckl

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,5 +1,8 @@
 # Create a variable 'num_one'  and assign it the value of the int 5.
-
+x=5
+y=8
+z=x+y
+print(z)
 # Create a variable 'num_two'   and assign it the value of the int 4.
 
 # Create a variable 'variable_total' and assign it the sum of  'num_one' and 'num_two'.
