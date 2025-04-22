d1b90baf82eb1bc54e29627657fd0be1f3e450c7
jaloliddinqurbonov <qurbonovjaloliddin2003@gmail.com>
2025-04-22 14:16:16 +0500

    uy ishiga kritilgan so'ngi o'zgartirish

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,6 +1,6 @@
 # Create a variable 'num_one'  and assign it the value of the int 5.
 num_one=5
-num_two=8
+num_two=4
 variable_total=num_one+num_two
 print(variable_total)
 # Create a variable 'num_two'   and assign it the value of the int 4.
